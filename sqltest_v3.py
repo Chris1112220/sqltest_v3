7bdbadadf615edc528068383bfb0b4f190d9c993
Chris1112220 <Christopher.roberts11220@gmail.com>
2025-03-11 11:49:25 -0400

    Addded Transactions table and printed out data to output

--- sqltest_v3.py
+++ sqltest_v3.py
@@ -16,6 +16,14 @@ CREATE TABLE IF NOT EXISTS Sales (
     sale_date TEXT NOT NULL
 );
 """)
+# Creating new Tranactions table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Transactions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    transaction_date TEXT NOT NULL,
+    amount REAL NOT NULL
+);
+""")
 
 
 # inserting sample data
@@ -26,6 +34,20 @@ sample_data = [
     ("Monitor", 2, 300.00, "2025-03-04"),
     ("Headphones", 7, 50.00, "2025-03-05")
 ]
+
+
+# Inserting sample data into Transactions table
+sample_transactions = [
+    ("2025-03-01", 1500.00),
+    ("2025-03-02", 250.50),
+    ("2025-03-02", 600.00),
+    ("2025-03-03", 75.99),
+    ("2025-03-05", 300.00)
+]
+cursor.executemany(
+    "INSERT INTO Transactions (transaction_date, amount) VALUES (?, ?)", sample_transactions)
+
+
 cursor.executemany(
     "INSERT INTO Sales (product_name, quantity, price, sale_date) VALUES (?, ?, ?, ?)", sample_data)
 conn.commit()
@@ -36,6 +58,10 @@ cursor.execute("SELECT * FROM Sales")
 for row in cursor.fetchall():
     print(row)
 
+print("\nAll Transactions:")
+cursor.execute("SELECT * FROM Transactions")
+for row in cursor.fetchall():
+    print(row)
 
 print("\nSales with price > $100:")
 cursor.execute("SELECT * FROM Sales WHERE price > 100")
